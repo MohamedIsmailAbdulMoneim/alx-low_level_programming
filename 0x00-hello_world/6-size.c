caf5b7f662d00f300ccf30d2e22897e200760af6
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-08 15:10:21 -0800

    s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	float z;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b);
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n"(unsigned long)sizeof(x));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z);
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(x));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 	return (0);
 }
