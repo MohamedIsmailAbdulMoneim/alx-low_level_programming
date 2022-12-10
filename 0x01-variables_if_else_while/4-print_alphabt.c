e36038c7e6000072eb16cb295a82d3ebfe6c0654
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-10 03:47:14 -0800

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		if (l != 'q' && l != 'e')
+			putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
