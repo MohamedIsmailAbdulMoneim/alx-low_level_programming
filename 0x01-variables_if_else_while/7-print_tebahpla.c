b4e2fda5f35a0a6d912e47b28d282a49f5173745
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-10 04:14:35 -0800

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	l = 'z';
+	while (l <= 'a')
+	{
+		putchar(l);
+		l--;
+	}
+	putchar('\n');
+	return (0);
+}
