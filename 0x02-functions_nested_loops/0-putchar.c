f33c15b28ee21796240e2887cb87a7e3a2200426
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-14 09:03:08 -0800

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
 #include "main.h"
 /**
- * main - Entry
+ * main - print _putchar
+ * is written by mia
  * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
