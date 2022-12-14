8e653693e596090ff6d8608a00b92cdc95f922fb
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-14 09:12:15 -0800

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
-#include <unistd.h>
-#include "main.h"
+#include <stdio.h>
 /**
  * main - print _putchar
  * is written by mia
- *  _putchar - writes the character c to stdout
  * Return: 0
  */
 int main(void)
 {
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
