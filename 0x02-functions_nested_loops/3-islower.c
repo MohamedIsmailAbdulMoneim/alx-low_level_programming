3f4baf1cd44c9f8834badfc9ca309883c8bed97c
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-14 14:07:10 -0800

    s

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 /**
  * _islower - returns 1 if true and 0 if false
  */
- 
- int _islower(int c)
- {
+int _islower(int c)
+{
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
-       	{
+	{
 		return (0);
-	}
- }
+	}	
+}
 
