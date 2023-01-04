affc96ccd6b6a814758f3ece3eb87cfa852242f6
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-01-04 21:04:33 +0200

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s:string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+  if(*s)
+  {
+    putchar("%c", *s)
+    _puts_recursion(s + 1)
+  }
+  else
+    return;
+}
