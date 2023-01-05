aaa30dd59ef9608ce0bb2593997f968ed834fd92
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-01-05 20:38:02 +0200

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s:string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+  if(*s)
+  {
+    _print_rev_recursion(s + 1)
+    putchar("%c", *s)
+  }
+}
