75e9d038400c049e941f58c8b5d0f50eae6d8c09
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-01-10 23:16:02 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,13 @@
 char* create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	if (size == 0)
+		return (NULL);
 	char* arr = (char*) malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		if(arr == NULL)
-		{
-			fprintf(stderr, "This is an error message.\n");
-		}
+			return (NULL);
 		arr[i] = c;
 	}
 	return arr;
