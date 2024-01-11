9049d1e1ae8e7a1f6552601f41022133dc0de8be
Muhamed Khodair <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2024-01-11 18:11:50 +0200

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void _puts_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        printf("%c", *s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        printf("\n");
+    }
+}
