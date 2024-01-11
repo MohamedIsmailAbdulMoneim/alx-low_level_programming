f35687619fce8d2129def09744038e72c9f70dff
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2024-01-11 11:48:20 -0800

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		if (*s == '\n') continue;
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		if(*s != '\n') _putchar(*s);
 	}
 	else
 	{
