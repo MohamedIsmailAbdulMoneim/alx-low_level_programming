42502814f7be3b51fcf9c5d75b64512a0142d66b
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2024-01-11 12:45:29 -0800

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,13 @@
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		len += 1;
-		_strlen_recursion(s + 1);
+		return (0);
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
 	}
 	return len;
 }
